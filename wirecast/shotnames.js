6c115902b76e3c82211c1f8b66f90fe0fb00ed64
Dean Bisogno <dean@notcoffeetable.com>
2013-03-25 23:01:54 -0700

    shotnames implemented in js.

--- wirecast/shotnames.js
+++ wirecast/shotnames.js
@@ -1,5 +1,26 @@
 //Return an object with shots in parent layer
 //wirecast.layer.shotnames();
-exports.shotnames = function (){
-    return {};
+// try {
+//     var win32ole = require('win32ole');
+//     var wirecast = win32ole.client.Dispatch('Wirecast.Application');
+//     var layer = wirecast.DocumentByIndex(1).LayerByName('Normal');
+//     shotnames(wirecast.DocumentByIndex(1), layer);
+// }catch(e){
+//     console.log('**Error caught***\n' + e);
+// }
+
+exports.shotnames = function(document, layer){
+//function shotnames(document, layer) {
+    try {
+        var shotcount = layer.ShotCount;
+        var shots = {}
+        for(var i = 1; i <= shotcount; i++) {
+            shots[i] = document.ShotByShotID(layer.ShotIDByIndex(i)).Name;
+            //console.log("shot " + i + ":", document.ShotByShotID(layer.ShotIDByIndex(i)).Name);
+        }
+    }catch(e){
+        console.log('**Error caught***\n' + e);
+    }
+    console.log(shots);
+    return shots;
 }
--- wirecast/wirecast.js
+++ wirecast/wirecast.js
@@ -1,6 +1,11 @@
 //Dependencies
 var win32ole = require('win32ole');
 
+try {
+    var wirecast = win32ole.client.Dispatch('Wirecast.Application');
+}catch(e){
+    console.log('*** exception caught ***\n' + e);
+}
 exports = {
     broadcast: require('broadcast').broadcast;
     record: exports.record = require('record').record;
